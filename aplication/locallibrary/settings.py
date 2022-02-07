8561b0beb7ff3e75957ea1e991f84073ecd65326
José <josealessan@gmail.com>
2022-02-07 08:27:15 -0300

    version 72

--- aplication/locallibrary/settings.py
+++ aplication/locallibrary/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFileStorage'
 
 # Redirect to home URL after login (default redirects to /accounts/profile/
 
